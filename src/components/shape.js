c9340b71e94b363a8a1f783ec0e54eaad2acc3bd
Simran Bhambri <93468452+bhambriSimran@users.noreply.github.com>
2021-11-22 23:48:33 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
+class Shape{
+  constructor(color){
+    this.color= 'color';
+  }
 
+     drawShape(){
+    console.log('Shape');
+     }
+  calculateArea(){
+    console.log('Area');
+     }
+}
+    
 // export class using module.exports
+  module.exports = Shape;
+//
