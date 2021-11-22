3be7171a63a26bcbb2038b1b29588c519c830738
Simran Bhambri <93468452+bhambriSimran@users.noreply.github.com>
2021-11-22 23:47:53 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,16 @@
 // import using require
+const Shape =require("./shape")
 
 // declare class
+class Circle extends Shape{
+  super(radius){
+    this.radius= radius;
+  }
 
-
+     calculateArea(){
+    console.log('Area');
+     }
+  }
+    
 // export class using module.exports
+  module.exports = Circle;
